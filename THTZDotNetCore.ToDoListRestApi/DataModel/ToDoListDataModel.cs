7e7a4c4c9e7c7d1d31f94d680587a6560d0f898d
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-10-27 22:01:04 +0630

    feat: todolist update, get,delete

--- THTZDotNetCore.ToDoListRestApi/DataModel/ToDoListDataModel.cs
+++ THTZDotNetCore.ToDoListRestApi/DataModel/ToDoListDataModel.cs
@@ -4,13 +4,13 @@
     {
         public int TaskId { get; set; }
         public string TaskName { get; set; } = null!;
-        public string? TaskDescritpion { get; set; }
+        public string? TaskDescription { get; set; }
         public int? TaskCategoryId { get; set; }
         public byte? PriorityLevel { get; set; }
         public string? Status { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? DueDate { get; set; }
         public DateTime? CompletedDate { get; set; }
-        
+        public bool DeleteFlag { get; set; }
     }
 }
