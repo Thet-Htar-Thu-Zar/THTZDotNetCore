1722bea280cd16c628b7647f104ddac4e5365146
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-11-01 18:35:27 +0630

    feat: adodotnet (edit)

--- THTZDotNetCore.ConsoleApp/AdoDotNet2.cs
+++ THTZDotNetCore.ConsoleApp/AdoDotNet2.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using THTZDotNetCore.Shared;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace THTZDotNetCore.ConsoleApp
 {
@@ -36,5 +37,90 @@ namespace THTZDotNetCore.ConsoleApp
 
             }
         }
+
+        public void Edit()
+        {
+            Console.Write("Blog Id: ");
+            string id = Console.ReadLine()!;
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where BlogId = @BlogId";
+
+            var dt = _adoDotNetService.Query(query, new SqlParameterModel("@BlogId", id));
+
+            DataRow dr = dt.Rows[0];
+            Console.WriteLine(dr["BlogId"]);
+            Console.WriteLine(dr["BlogTitle"]);
+            Console.WriteLine(dr["BlogAuthor"]);
+            Console.WriteLine(dr["BlogContent"]);
+        }
+
+        public void Create() 
+        {
+            Console.WriteLine("Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Blog Content: ");
+            string content = Console.ReadLine();
+
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+           ([BlogTitle]
+           ,[BlogAuthor]
+           ,[BlogContent]
+           ,[DeleteFlag])
+     VALUES
+            (@BlogTitle
+            ,@BlogAuthor
+            ,@BlogContent
+            ,0)
+            ";
+
+            int result = _adoDotNetService.Excute(query,
+                new SqlParameterModel("@BlogTitle", title),
+                new SqlParameterModel("@BlogAuthor", author),
+                new SqlParameterModel("@BlogContent", content));
+
+            Console.WriteLine(result == 1 ? "Saving Successful." : "Saving Fail.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Blog Id: ");
+            string id = Console.ReadLine();
+
+            Console.WriteLine("Blog Title: ");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Blog Author: ");
+            string author = Console.ReadLine();
+
+            Console.WriteLine("Blog Content: ");
+            string content = Console.ReadLine();
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+   SET [BlogTitle] = @BlogTitle
+      ,[BlogAuthor] = @BlogAuthor
+      ,[BlogContent] = @BlogContent
+      ,[DeleteFlag] = 0
+ WHERE BlogId = @BlogId";
+
+           
+            int result = _adoDotNetService.Excute(query,
+                new SqlParameterModel("@BlogId", id),
+                new SqlParameterModel("@BlogTitle", title),
+                new SqlParameterModel("@BlogAuthor", author),
+                new SqlParameterModel("@BlogContent", content));
+
+            Console.WriteLine(result == 1 ? "Updating Successful." : "Updating Fail.");
+        }
+
+        
     }
 }
