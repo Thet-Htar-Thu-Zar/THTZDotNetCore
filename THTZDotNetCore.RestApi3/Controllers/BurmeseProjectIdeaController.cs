788f0b80eb261045a33d6bfeff9f0b5a348d242f
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-29 21:41:26 +0630

    -

---
+++ THTZDotNetCore.RestApi3/Controllers/BurmeseProjectIdeaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace THTZDotNetCore.RestApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BurmeseProjectIdeaController : ControllerBase
+    {
+    }
+}
