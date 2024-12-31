794b8428df34b45e077fd9144dfbe0f2b0047e0d
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2024-12-31 14:50:51 +0630

    ApiDomainUrl

--- THTZDotNetCore.RestApi3/Program.cs
+++ THTZDotNetCore.RestApi3/Program.cs
@@ -1,3 +1,6 @@
+using Refit;
+using RestSharp;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton(n => new HttpClient()
+{
+    BaseAddress = new Uri(builder.Configuration.GetSection("ApiDomainUrl").Value!)
+});
+builder.Services.AddSingleton(n =>
+new RestClient(builder.Configuration.GetSection("ApiDomainUrl").Value!));
+builder.Services
+    .AddRefitClient<ISnakeApi>()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("ApiDomainUrl").Value!));
 
 var app = builder.Build();
 
@@ -23,3 +36,20 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public interface ISnakeApi
+{
+    [Get("/snakes")]
+    Task<List<ISnakeApi>> GetSnakes();
+}
+
+public class SnakeModel
+{
+    public int Id { get; set; }
+    public string ImageUrl { get; set; }
+    public string MMName { get; set; }
+    public string EngName { get; set; }
+    public string Detail { get; set; }
+    public string IsPoison { get; set; }
+    public string IsDanger { get; set; }
+}
