258725ac5144e2f31b5ab1aa3cf08d235449b9f7
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-01-04 23:10:59 +0630

    create database

---
+++ THTZDotNetCore.SnakeLadderGameApi/Controllers/SnakeLadderGameController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace THTZDotNetCore.SnakeLadderGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnakeLadderGameController : ControllerBase
+    {
+    }
+}
